3714fa4ad682aaac5ec248ef8d6bcdcdfb4d1555
Barry Yap <yapweiminbarry@gmail.com>
2024-10-01 23:57:16 +0800

    added differential testing for huff and sol

---
+++ test/v1/Base_TestV1.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License Identifier: GPL-3.0-only
+pragma solidity 0.8.26;
+
+import {HorseStore} from "../../src/horseStoreV1/horseStore.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+abstract contract Base_TestV1 is Test {
+    HorseStore public horseStore;
+
+    function setUp() public virtual {
+        horseStore = new HorseStore();
+    }
+
+    function testReadValue() public {
+        uint256 initialValue = horseStore.readNumberOfHorses();
+        assertEq(initialValue, 0);
+    }
+}
